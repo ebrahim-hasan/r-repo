ae9bcc2c26b50f11d3fe8f0097ecc9c93a3fe308
Ebrahim Hasan <Ebrahim.Hasan.dev@gmail.com>
2026-04-06 10:42:39 +0200

    Initial Commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RemoteProject
+{
+    public class Class1
+    {
+        int x = 11;
+        int x2 = 22; // this perform in remote project
+        int x3 = 33; // this perform in remote project
+        int x4 = 44; // this perform in remote project
+        int x6 = 66; // this perform in remote project
+        int x7 = 77; // this perform in remote project
+    }
+}
