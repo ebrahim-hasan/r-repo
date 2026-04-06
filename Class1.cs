2c2b47dfcb55f3fa6885a9ab3be4245642ae2719
Ebrahim Hasan <Ebrahim.Hasan.dev@gmail.com>
2026-04-06 12:15:41 +0200

    delete

--- Class1.cs
+++ Class1.cs
@@ -4,10 +4,5 @@ namespace RemoteProject
     public class Class1
     {
         int x = 11;
-        int x100 = 100;
-        int x200 = 200;
-        int x300 = 300;
-        int x400 = 400;
-        int x500 = 500;
     }
 }
